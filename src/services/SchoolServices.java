95a458bea2eaabb4bd74ee5c00ce14ccfb03a9b6
Oziichukwu <ukaegbugoodnews13@gmail.com>
2021-10-01 16:39:18 +0100

    school system

--- src/services/SchoolServices.java
+++ src/services/SchoolServices.java
@@ -1,2 +1,15 @@
-package services;public interface SchoolService {
+package services;
+
+import exceptions.SchoolNotFoundException;
+import models.School;
+
+import java.util.List;
+
+public interface SchoolServices {
+
+    void add(School school);
+    void delete(School school);
+    School update(long id) throws SchoolNotFoundException;
+    School findById(long id);
+    List<School> getAll();
 }
