fb0262529b55ec6fb87b0cd7d674a20668781fd0
Oziichukwu <ukaegbugoodnews13@gmail.com>
2021-10-16 14:14:50 +0100

    school management system

--- src/schoolSystemApp/SchoolSystemApp.java
+++ src/schoolSystemApp/SchoolSystemApp.java
@@ -3,6 +3,20 @@ package schoolSystemApp;
 public class SchoolSystemApp {
     public static void main(String[] args) {
 
-
+        methodC();
     }
+
+    public static void methodA(){
+        System.out.println("This is data in method A");
+    }
+    public static void methodB(){
+        methodA();
+        System.out.println("This is data in method B");
+    }
+    public static void methodC(){
+        methodB();
+        System.out.println("This is data in method C");
+    }
+
 }
+
